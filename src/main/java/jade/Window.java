b45c7fc6498f4adc91a01ced6c31c4dbef9dad72
ThibaltM <thibalt.meurin@gmail.com>
2023-03-21 11:47:30 +0100

    initial commit

---
+++ src/main/java/jade/Window.java
@@ -0,0 +1,78 @@
+package jade;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    int width, height;
+    private String title;
+    private long glfwWindow;
+
+    private static Window window = null;
+    private Window(){
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Mario";
+    }
+
+    public static Window get(){
+        if(Window.window == null){
+            Window.window = new Window();
+        }
+        return Window.window;
+    }
+
+    public void run(){
+        System.out.println("Hello LWJGL " + Version.getVersion());
+
+        init();
+        loop();
+    }
+
+    public void init(){
+        //error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        //initializa GLF
+        if(!glfwInit()){
+            throw new IllegalStateException("Unable to initialize glfw");
+        }
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        //create window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL,NULL );
+
+        if(glfwWindow == NULL){
+            throw new IllegalStateException("Failed to create GLFW window");
+        }
+
+        glfwMakeContextCurrent(glfwWindow);
+
+        glfwSwapInterval(1);
+
+        //show window
+        glfwShowWindow(glfwWindow);
+
+        GL.createCapabilities();
+    }
+
+    public void loop(){
+        while(!glfwWindowShouldClose(glfwWindow)){
+            glfwPollEvents();
+            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(glfwWindow);
+        }
+    }
+}
